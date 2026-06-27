e5e3422f055c541fdc5a165772b17f785378c73d
shradha005 <shandilyashradha140@gmail.com>
2026-06-27 23:44:28 +0530

    "updated"

--- src/SLowAndFast_Pattern/OneDeletionKadanes.java
+++ src/SLowAndFast_Pattern/OneDeletionKadanes.java
@@ -3,7 +3,8 @@ package SLowAndFast_Pattern;
 public class OneDeletionKadanes {
     public static void main(String[] args)
     {
-        int[] a = {1,-2,0,3} ;
+//        int[] a = {1,-2,0,3} ;
+        int[] a = {-1,-1,-1,-1} ;
         int ans = OneDelete(a) ;
         System.out.print(ans) ;
     }
@@ -12,18 +13,19 @@ public class OneDeletionKadanes {
     {
         int n= arr.length ;
         int power = 0;
-        int nopower = 0;
-        int res  =0;
+        int nopower = arr[0];
+        int res  = arr[0];
 
-        for(int i =0; i<n ; i++)
+        for(int i =1; i<n ; i++)
         {
             int v1 = arr[i] ;
             int v2  = nopower + arr[i] ;
-            int v3 = power;
-            int v4 = power + arr[i] ;
+            int v3 = power + arr[i];
+            int v4 = nopower ;
             nopower = Math.max(v1,v2) ;
             power = Math.max ( v3,v4) ;
-            res = Math.max(power,nopower) ;
+            res = Math.max(res , Math.max(nopower,power)) ;
+
 
         }
         return res ;
