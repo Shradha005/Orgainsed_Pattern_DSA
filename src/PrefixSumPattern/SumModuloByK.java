ff78b479f30e22f2229654d83c637d0ea1672b1b
shradha005 <shandilyashradha140@gmail.com>
2026-07-05 18:28:57 +0530

    updated

--- src/PrefixSumPattern/SumModuloByK.java
+++ src/PrefixSumPattern/SumModuloByK.java
@@ -4,19 +4,32 @@ import java . util .* ;
 public class SumModuloByK {
     public static void main(String[] args )
     {
-
+        int[] arr = {4,5,0,-2,-3,1} ;
+        int k = 5;
+        int ans = SumModulo(arr,k) ;
+        System.out.print(ans) ;
     }
-    public static void SumModulo(int [] a , int k )
+    public static int SumModulo(int [] a , int k )
     {
         int sum = 0;
         int res = 0;
+        int rem = 0;
         int n = a.length ;
         Map<Integer, Integer> freq = new HashMap<>() ;
         freq.put(0,1) ;
         for (int i =0 ;i<n;i++)
         {
-            sum += sum % k ;
+            sum +=  a[i] ;
+            rem = sum % k ;
+            if(rem < 0)
+            {
+                rem = rem  + k ;
+            }
 
+
+            res += freq.getOrDefault(rem,0) ;
+            freq.put(rem,freq.getOrDefault(rem,0) + 1) ;
         }
+        return res ;
     }
 }
